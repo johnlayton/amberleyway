f11d25a28223170950665c706bf7bfb321850ccd
John Layton <john.layton@mebank.com.au>
2020-03-03 20:54:35 +1100

    initial commit of project files

---
+++ build.gradle.kts
@@ -0,0 +1,93 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+buildscript {
+  repositories {
+    mavenLocal()
+    mavenCentral()
+    maven("https://jitpack.io")
+  }
+  dependencies {
+    classpath("au.com.mebank.integration:riverhilldrive:0.0.1-3-gf2aa65a")
+  }
+  configurations {
+    classpath {
+      resolutionStrategy {
+        cacheChangingModulesFor(0, "seconds")
+      }
+    }
+  }
+}
+
+plugins {
+//  base
+//  java
+//  publishing
+
+  `kotlin-dsl`
+  `maven-publish`
+
+  kotlin("jvm") version "1.3.61" apply false
+  kotlin("kapt") version "1.3.61" apply false
+
+  kotlin("plugin.spring") version "1.3.61" apply false
+
+  id("io.spring.dependency-management") version "1.0.8.RELEASE" apply false
+  id("org.springframework.boot") version "2.2.0.RELEASE" apply false
+
+  id("com.google.cloud.tools.jib") version "2.0.0" apply false
+
+  // Local plugins
+//  id("plugin-version")
+//  id("plugin-group")
+}
+
+//repositories {
+//  mavenLocal()
+//  mavenCentral()
+//  maven("https://jitpack.io")
+//}
+
+allprojects {
+//
+  apply(plugin = "base")
+  apply(plugin = "java")
+//
+  apply(plugin = "plugin-version")
+  apply(plugin = "plugin-group")
+
+
+  repositories {
+    jcenter()
+    mavenLocal()
+    mavenCentral()
+    maven("https://jitpack.io")
+  }
+//
+  dependencies {
+    implementation(kotlin("stdlib-jdk8", "1.3.61"))
+  }
+//
+//  tasks.withType<Test> {
+//    useJUnitPlatform()
+//  }
+//
+//  java {
+//    sourceCompatibility = JavaVersion.VERSION_11
+//    targetCompatibility = JavaVersion.VERSION_11
+//  }
+//
+  tasks.withType<KotlinCompile> {
+    kotlinOptions {
+      freeCompilerArgs = listOf("-Xjsr305=strict")
+      jvmTarget = "11" // JavaVersion.VERSION_11
+    }
+  }
+}
+
+val gradleWrapperVersion: String by project
+tasks {
+  wrapper {
+    gradleVersion = gradleWrapperVersion
+    distributionType = Wrapper.DistributionType.ALL
+  }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+  repositories {
+    gradlePluginPortal()
+    mavenLocal()
+    mavenCentral()
+    jcenter()
+    maven("https://plugins.gradle.org/m2/")
+  }
+  resolutionStrategy {
+    eachPlugin {
+      if (requested.id.id == "com.google.protobuf") {
+        useModule("com.google.protobuf:protobuf-gradle-plugin:${requested.version}")
+      }
+    }
+  }
+}
+
+rootProject.name = "amberleyway"
+
+include("app", "api", "sal", "model")
