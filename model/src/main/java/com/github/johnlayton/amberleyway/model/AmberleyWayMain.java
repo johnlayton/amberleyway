844728dd49fbd81cff07e34c3a2342ad120b6985
John Layton <johnstewartlayton@gmail.com>
2020-08-13 14:12:38 +1000

    add spotbugs error

--- model/src/main/java/com/github/johnlayton/amberleyway/model/AmberleyWayMain.java
+++ model/src/main/java/com/github/johnlayton/amberleyway/model/AmberleyWayMain.java
@@ -4,7 +4,11 @@ public class AmberleyWayMain {
 
   public static void main(String[] args) {
     try {
-
+      int val = 1;
+      switch(val) {
+        case (1):
+          break;
+      }
     } catch (Exception exception) {
 
     }
